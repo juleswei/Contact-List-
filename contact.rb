e8a27c485944a962c458191254ead84d589d583f
Jules Wei <s.jules2000@gmail.com>
2016-03-08 20:47:45 +0000

    edited version of contact list

--- contact.rb
+++ contact.rb
@@ -1,28 +1,26 @@
 require 'csv'
-
-# Represents a person in an address book.
-# The ContactList class will work with Contact objects instead of interacting with the CSV file directly
+require 'pry'
 
 class Contact
 
   attr_accessor :name, :email
-  
-  # Creates a new contact object
-  # @param name [String] The contact's name
-  # @param email [String] The contact's email address
+
   def initialize(name, email)
     @name = name
     @email= email
-    # TODO: Assign parameter values to instance variables.
   end
   class << self
 
     def initial_message
-      puts " Here is a list of available commands:"
-      puts "    new     -Create a new contact"
-      puts "    list    -list all contacts"
-      puts "    show    - Show a contact"
-      puts "    search  - Search contacts"
+      menu = %{
+        Here is a list of available commands:
+              new     -Create a new contact
+              list    -list all contacts
+              show    - Show a contact
+              search  - Search contacts
+      }
+
+      puts menu
 
       response = gets.chomp.downcase
       if response == "new"
@@ -37,11 +35,11 @@ class Contact
       elsif response == "show"
         puts "What's the number(id) you want to search for?"
         id = gets.chomp.to_i
-        find(id)
+        puts find(id) ? find(id) : "not found"
       elsif response == "search"
         puts "Type in a keyword of the contact you are trying to search for"
         term = gets.chomp.downcase
-        search(term)     
+        puts search(term).join(', ')     
       else
         puts" that's not a valid input, can you type it again?"    
       end
@@ -58,32 +56,24 @@ class Contact
         csv << [name_input, email_input]
       end
     end
-    # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
+
     def find(id)
       index = 1
       CSV.foreach('contacts.csv','r') do |row|
         if index == id
-          puts "contact is #{row}"
-          # return row
+          return "contact is #{row}"
         else
-          puts "not found"
+          return nil
         end
         index += 1 
       end
     end
-    
-    # Search for contacts by either name or email.
-    # @param term [String] the name fragment or email fragment to search for
-    # @return [Array<Contact>] Array of Contact objects.
+
     def search(term)
-      CVS.foreach('contacts.csv', 'r') do |row|
-          row.each do |term|
-            if include? term
-              puts row ['term']
-            end
-          end
+      CSV.foreach('contacts.csv', 'r') do |row|
+        
+        return row if row.join(',').downcase.include? term.downcase  
       end
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
     end
 
   end
